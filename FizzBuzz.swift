2be753239199ab9eb42676d1bf2f9d8cd8d00025
Don Bytyqi <donbytyqiapps@gmail.com>
2017-08-24 19:24:09 +0200

    Update FizzBuzz.swift

--- FizzBuzz.swift
+++ FizzBuzz.swift
@@ -15,3 +15,29 @@ func fizzBuzzGame(upTo: Int) {
 }
 
 fizzBuzzGame(upTo: 20)
+
+
+// I started learning Java, here's the Java version:
+
+public static void fizzBuzzGame(int upTo) {
+		
+		for (int i=1; i<upTo; i++) {
+			
+			String value = "";
+			
+			if ((i % 3) == 0) {
+				value += "Fizz ";
+			}
+			
+			if ((i % 5) == 0) {
+				value += "Buzz";
+			}
+			
+			if (value.isEmpty()) {
+				value += String.valueOf(i);
+			}
+			
+			System.out.println(value);
+		}
+		
+	}
